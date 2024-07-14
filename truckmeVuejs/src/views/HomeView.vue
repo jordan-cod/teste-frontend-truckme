be202720c122410a44388c0f493542af78f4b6cb
Gabriel Jordan <gabrijordan3@gmail.com>
2024-07-14 15:38:56 -0300

    Utilizar novos componentes criados

--- truckmeVuejs/src/views/HomeView.vue
+++ truckmeVuejs/src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import BannerComponent from '@/components/BannerComponent.vue';
 import AboutComponent from '@/components/AboutComponent.vue';
+import ServicesTableComponent from '@/components/ServicesTableComponent.vue';
+import ContactFormComponent from '@/components/ContactFormComponent.vue';
+import GalleryComponent from '@/components/GalleryComponent.vue';
+
 </script>
 
 <template>
   <main>
     <BannerComponent/>
     <AboutComponent/>
+    <ServicesTableComponent/>
+    <ContactFormComponent/>
+    <GalleryComponent/>
   </main>
 </template>
