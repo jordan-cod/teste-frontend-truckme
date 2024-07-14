19b638f2d1594e8ae1250e39d011597e801f0cba
Gabriel Jordan <gabrijordan3@gmail.com>
2024-07-14 15:38:28 -0300

    Utiizar componente footer no layout

--- truckmeVuejs/src/App.vue
+++ truckmeVuejs/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
   <div>
     <HeaderComponent />
     <RouterView />
+    <FooterComponent/>
   </div>
 </template>
 
