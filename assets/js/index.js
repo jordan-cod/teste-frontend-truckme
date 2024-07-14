fdb0525484899ce78da3a506331c504952365dc3
Gabriel Jordan <gabrijordan3@gmail.com>
2024-07-13 22:54:08 -0300

    Finalizar seção de serviços
    - Alterar estrutura de pastas dos assets
    - Alterar a qualidade de algumas imagens
    - Alterar a logica do javascript do menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
     const button = document.querySelector('.hamburger-lines');
     const menu = document.querySelector('.menu');
+    const links = document.querySelectorAll('.link');
 
     button.addEventListener('click', () => {
         menu.classList.toggle('active');
         button.classList.toggle('active');
-        menuToggle.classList.toggle('active');
     });
-});
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+            button.classList.remove('active');
+        });
+    })
