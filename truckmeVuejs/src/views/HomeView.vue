08ebbed700022ea71a6691b00519fd53fbc7600e
Gabriel Jordan <gabrijordan3@gmail.com>
2024-07-14 12:50:54 -0300

    Utilizar componentes banner e about

--- truckmeVuejs/src/views/HomeView.vue
+++ truckmeVuejs/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import BannerComponent from '@/components/BannerComponent.vue';
+import AboutComponent from '@/components/AboutComponent.vue';
 </script>
 
 <template>
   <main>
+    <BannerComponent/>
+    <AboutComponent/>
   </main>
 </template>
