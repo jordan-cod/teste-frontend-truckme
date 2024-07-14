e9499d03aaea4bb66376f5bccd71c0bb335f27b5
Gabriel Jordan <gabrijordan3@gmail.com>
2024-07-14 13:29:48 -0300

    Criar componente para seção de serviços e criar componente para serviço

---
+++ truckmeVuejs/src/components/ServiceCardComponent.vue
@@ -0,0 +1,76 @@
+<template>
+  <article class="card">
+    <div>
+      <div class="icon-container"><component :is="props.icon" /></div>
+      <div>
+        <h3>{{ props.title }}</h3>
+        <p>{{ props.description }}</p>
+      </div>
+    </div>
+    <div>
+      <span>R$ {{ props.value }}</span>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Component } from 'vue'
+
+const props = defineProps({
+  icon: Object as () => Component,
+  title: String,
+  description: String,
+  value: Number
+})
+</script>
+
+<style scoped>
+.card {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: white;
+    padding: 20px 25px;
+    border-radius: 15px;
+    color: #1B252D;
+}
+.card > div:first-child {
+    display: flex;
+    align-items: center;
+    column-gap: 30px;
+}
+.card > div:first-child > div:last-child {
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+}
+.card > div:first-child > div:last-child h3 {
+    font-size: 1.8rem;
+}
+.card > div:first-child > div:last-child p {
+    font-size: 1.1rem;
+}
+.card > div:last-child {
+    font-size: 2rem;
+    font-weight: 700;
+    color: #F05123;
+    padding: 0px 20px;
+    width: 180px;
+}
+.icon-container svg {
+    width: 150px;
+}
+@media (max-width: 920px) {
+    #services .card{
+        flex-direction: column;
+        row-gap: 20px;
+        padding: 30px;
+    }
+    #services .card > div:first-child{
+        flex-direction: column;
+        text-align: center;
+        row-gap: 20px;
+    }
+}
+</style>
---
+++ truckmeVuejs/src/components/ServicesTableComponent.vue
@@ -0,0 +1,108 @@
+<template>
+    <section id="services">
+      <div>
+        <h2>Serviços</h2>
+  
+        <ServiceCard
+          v-for="service in services"
+          :key="service.title"
+          :icon="service.icon"
+          :title="service.title"
+          :description="service.description"
+          :value="service.value"
+        />
+      </div>
+    </section>
+  </template>
+
+<script setup lang="ts">
+import type { Component } from 'vue'
+
+import ServiceCard from './ServiceCardComponent.vue'
+
+import PadlockIcon from './icons/PadlockIcon.vue'
+import ComplianceIcon from './icons/ComplianceIcon.vue'
+import ContractIcon from './icons/ContractIcon.vue'
+import ContractSearchIcon from './icons/ContractSearchIcon.vue'
+
+type Service = {
+  icon: Component
+  title: string
+  description: string
+  value: number
+}
+
+const services: Array<Service> = [
+  {
+    icon: PadlockIcon,
+    title: 'Segurança da Carga',
+    description:
+      'Garantimos a integridade das suas mercadorias durante todo o processo de transporte.',
+    value: 800
+  },
+  {
+    icon: ComplianceIcon,
+    title: 'Compliance em Negociações',
+    description:
+      'Nossas operações seguem rigorosamente todas as normas e regulamentos, assegurando transparência.',
+    value: 900
+  },
+  {
+    icon: ContractIcon,
+    title: 'Contratos de Frete',
+    description:
+      'Oferecemos contratos de frete claros e justos, adaptados às necessidades de cada cliente.',
+    value: 1000
+  },
+  {
+    icon: ContractSearchIcon,
+    title: 'Documentos Fiscais',
+    description:
+      'Gerenciamento completo dos documentos fiscais necessários para o transporte de cargas.',
+    value: 400
+  }
+]
+</script>
+
+<style scoped>
+#services {
+    width: 100%;
+    background-color: #1B252D;
+    padding: 75px 0px;
+}
+#services > div {
+    width: 1080px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+}
+#services h2 {
+    color: var(--truck-white);
+    font-size: 2rem;
+}
+@media (max-width: 1080px) {
+    #services > div {
+        width: 100%;
+    }
+    #services > div h2 {
+        text-align: center;
+    }
+    #services {
+        padding: 75px 15px;
+    }
+}
+@media (max-width: 920px) {
+    .card {
+        flex-direction: column;
+        row-gap: 20px;
+        padding: 30px;
+    }
+    .card > div:first-child {
+        flex-direction: column;
+        text-align: center;
+        row-gap: 20px;
+    }
+}
+</style>
